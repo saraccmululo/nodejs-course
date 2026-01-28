519c03b883c9a0e3f52b67124b25b7e6d63cb86e
Sara Costa Cabral Mululo <saramululo@gmail.com>
2026-01-28 17:10:13 -0600

    Remove node_modules from Git tracking

--- 2.3 NPM/index.js
+++
@@ -1,8 +0,0 @@
-import generateName from "sillyName"
-import {randomSuperhero} from 'superheroes';
-
-var sillyName = generateName();
-console.log(`My name is ${sillyName}.`);
-
-const superHeroeName=randomSuperhero();
-console.log(`I am ${superHeroeName}!`)
