114ceba6eb506715a9b820034f93967ab3d4558b
Sara Costa Cabral Mululo <saramululo@gmail.com>
2026-02-09 09:14:00 -0600

    use EJS templates

--- 5.6 Secrets Project/index.js
+++ 5.6 Secrets Project/index.js
@@ -1,13 +1,32 @@
 // HINTS:
 // 1. Import express and axios
+import express from "express";
+import axios from "axios";
 
 // 2. Create an express app and set the port number.
-
+const app= express();
+const port = 3000;
 // 3. Use the public folder for static files.
-
+app.use(express.static("public"));
 // 4. When the user goes to the home page it should render the index.ejs file.
-
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
 
-// 6. Listen on your predefined port and start the server.
+app.get("/", async (req, res)=> {
+  try{
+    const response = await axios.get(
+    "https://secrets-api.appbrewery.com/random",
+  );
+  const result=response.data;
+  res.render("index.ejs", {secret: result.secret, user: result.username})
+  } catch (error) {
+    console.error(error.response?.data||error.message);
+    res.status(500).send("500-something went wrong");
+  }
+});
+
+
+// 6. Listen on your predefined port and start the server.  
+app.listen(port, ()=> {
+  console.log(`Server listening on port ${port}`)
+});
